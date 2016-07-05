f9c7cec76ed0748d82e7377b12fec92024ec86e2
Dylan Pelletier <dylan.pelletier1979@gmail.com>
2016-07-05 17:49:53 +0000

    Finished the first two functions

--- answers.js
+++ answers.js
@@ -1,1 +1,25 @@
 // This is the first step in Workshop 1.
+
+
+// This function will reveal the first letter of the word and print it
+function firstLetter (word) {
+    console.log(word[0]);
+};
+
+firstLetter("Dylan");
+
+firstLetter("");
+
+firstLetter("Superman");
+
+// This function should print the last letter of the string
+
+function lastLetter(word) {
+    console.log(word[word.length - 1]);
+};
+
+lastLetter("Dylan");
+
+lastLetter("");
+
+lastLetter("Emma");
