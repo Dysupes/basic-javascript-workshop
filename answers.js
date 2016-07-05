bc33faa926491cfda817401022461413e5f3259e
Dylan Pelletier <dylan.pelletier1979@gmail.com>
2016-07-05 18:23:46 +0000

    Finished up to function 7

--- answers.js
+++ answers.js
@@ -14,8 +14,8 @@ firstLetter("Superman");
 
 // This function should print the last letter of the string
 
-function lastLetter(word) {
-    console.log(word[word.length - 1]);
+function lastLetter(mot) {
+    console.log(mot[mot.length - 1]);
 };
 
 lastLetter("Dylan");
@@ -27,8 +27,8 @@ lastLetter("Emma");
 /* This function takes a string and a number and print a character
     FYI: pono is Korean for number :) */
     
-function character(word,pono) {
-    console.log(word[pono]);
+function character(char,pono) {
+    console.log(char[pono]);
 };
 
 character("George",2);
@@ -48,3 +48,50 @@ addition(2,4);
 addition(465,978);
 
 addition(3,12);
+
+// This function takes two numbers and multiplies them (if input is not a number, NaN returns)
+
+function multiplication(numb1, numb2) {
+    console.log(numb1 * numb2);
+};
+
+multiplication(67,33);
+
+multiplication(200,965);
+
+multiplication(200,"Sam");
+
+// This function explores if and else
+
+function options(words,n1,n2) {
+    if (words === "add") {
+        console.log(n1 + n2);
+    } else if (words === "subtract") {
+        console.log(n1 - n2);
+    } else if (words === "mult") {
+        console.log(n1 * n2);
+    } else if (words === "div") {
+        console.log(n1 / n2);
+    } else {
+        console.log(0);
+    }
+};
+
+options("add",2,67);
+
+options("subtract",98,56);
+
+options("mult",56,87);
+
+options("div",98,7);
+
+// This function repeats a string to correspond to the number input
+
+function repeat(sent,mult1) {
+    for(var i = mult1; i > 0; i = i - 1){
+    console.log(sent);
+
+    };    
+};
+
+repeat("Javascript is fun!",4);
